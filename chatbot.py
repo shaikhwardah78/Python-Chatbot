a0d4a6cd9bb73a5ac9d84caa424b8a83ddfcf363
Warda Shaikh <ws2594574@gmail.com>
2026-08-30 20:47:05 +0500

    Update chatbot reply function and comments
    Refactor comments and improve greeting messages.

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,22 @@
-# I used def function (to decides what reply to give based on what the user said)
-# This is main chatting part for btw you and bot 
+# I used a def function (to decide what reply to give based on what the user said)
+# This is the main chatting part for btw you and the bot 
 def get_reply(user_message):
     message = user_message.lower()
     if "hello" in message or "hi" in message:
-        return "Hi Welcome!👋, How can i help you?"
+        return "Hi, welcome!👋 How can I help you?"
     elif "how are you?" in message:
         return "I'm doing great, thanks for asking! How about you?"
     elif "what is your name?" in message: 
         return "I'm Chatbot 1.0, your new Python friend!"
     elif "ok if i want any help then i will ask you. goodbye!" in message:
-        return "Yeah! Ok i will help you anytime. Goodbye!👋"
+        return "Yeah! Ok, I will help you anytime. Goodbye!👋"
 
 print("-"*40)
 print("🤖 Welcome to your first ChatBot!")
 print("-"*40)
-print("Type 'bye',If you want to exit the chatbot.\n")
+print("Type 'bye' if you want to exit the chatbot.\n")
 
-# This the loop part for chat end (I used while loop) and also input() function.
+# This is the loop part for chat end (I used a while loop) and also the input() function.
 while True:
     user_input = input("YOU:")
 
